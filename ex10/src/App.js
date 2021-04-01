a25360b04c16ba3e0352ea5ffb7e53b10cabd8b4
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-04-02 01:13:28 +0200

    adding ex folders

---
+++ ex10/src/App.js
@@ -0,0 +1,12 @@
+import DeleteRequest from './DeleteRequest';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DeleteRequest />
+    </div>
+  );
+}
+
+export default App;
